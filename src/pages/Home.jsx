dcedea2e43748015c87ab24e3c862e9c00dcee18
Kyle Greenidge <kylegreen149@gmail.com>
2025-07-29 01:31:23 -0400

    Use useState and conditional rendering to keep track of user's input in search field, and match the input with any of the movies that exist on the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("")
     const movies = [
         {id: 1, title: "Inception", release_date: 2010},
         {id: 2, title: "Interstellar", release_date: 2014},
@@ -8,10 +10,28 @@ function Home() {
         {id: 4, title: "Pulp Fiction", release_date: 1994}
     ]
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    }
+
     return (
         <div className="home">
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Search for movies..." 
+                className="search-input" 
+                onChange={(e) => {setSearchQuery(e.target.value)}}
+                value={searchQuery}/>
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
             <div className="movies-grid">
-                {movies.map(movie => <MovieCard movie={movie} key={movie.id}/> )}
+                {movies.map(movie => 
+                    movie.title.toLowerCase().includes(searchQuery) && (
+                        <MovieCard movie={movie} key={movie.id}/>
+                        ) 
+                )}
             </div>
         </div>
     )
