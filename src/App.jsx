abcbf4507c1ec9195f68d41cd76ea413bee80e13
Kyle Greenidge <kylegreen149@gmail.com>
2025-07-30 22:40:37 -0400

    Create navbar and set up links using Routes and Browser Router from react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
-import MovieCard from './components/MovieCard.jsx'
+import {Routes, Route} from "react-router-dom"
 import Home from "./pages/Home.jsx"
+import Favorites from "./pages/Favorites.jsx"
+import NavBar from './components/NavBar.jsx'
 function App() {
   return (
     <>
-      <Home />
+      <div>
+        <NavBar />
+        <main className='main-content'>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/favorites' element={<Favorites />}/>
+          </Routes>
+        </main>
+      </div>
     </>
   )
 }
