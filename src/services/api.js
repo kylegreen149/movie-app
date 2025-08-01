662bd439bb9c399bacc2f6ba489e63c336215bfd
Kyle Greenidge <kylegreen149@gmail.com>
2025-07-31 23:12:57 -0400

    Set up API by getting API key and creating functions that will help users search for movies

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "a2c192003a3d706cf4ed7883e1820b39"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
