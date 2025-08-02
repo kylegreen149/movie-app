3974eb0dd485f4656219e4cd70d493bdf9074ca7
Kyle Greenidge <kylegreen149@gmail.com>
2025-08-02 17:45:11 -0400

    Remove searchQuery lookups from movies-grid div so that the component re-renders upon form submission, not through the onChange event listener

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,11 +57,9 @@ function Home() {
 
             {loading ? (<div className="loading">Loading...</div>) : (
                 <div className="movies-grid">
-                    {movies.map(movie => 
-                        movie.title.toLowerCase().includes(searchQuery) && (
-                            <MovieCard movie={movie} key={movie.id}/>
-                            ) 
-                    )}
+                    {movies.map(movie => (
+                        <MovieCard movie={movie} key={movie.id}/>
+                    ))}
                 </div>
             )}
         </div>
